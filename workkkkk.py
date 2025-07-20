25f9dd8bf538b2e704f44980abaa222c40b79a23
Samuel Nkansah <snkansah259@gmail.com>
2025-07-20 10:55:28 +0100

    Add files via upload

---
+++ workkkkk.py
@@ -0,0 +1,19 @@
+# Welcome
+print("Hello! Let's get to know you a little better.\n")
+
+#  personal details
+name = input("What's your name? ")
+age = input("How old are you? ")
+colour = input("What's your favourite colour? ")
+food = input("What's your favourite food? ")
+school = input("What school did you attend? ")
+team = input("What is your favourite soccer team? ")
+
+# summary
+print("\n--- Customized Summary ---")
+print(f"Nice to meet you, {name}!")
+print(f"You are {age} years old, and your favourite colour is {colour}.")
+print(f"You love eating {food}, and you attended {school}.")
+print(f"Wow , your favourite team is {team}.")
+print("Thanks for sharing!")
+
